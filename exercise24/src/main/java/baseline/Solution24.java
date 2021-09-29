2326ca5afaafba2c2d9058c387501717e586f717
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-09-29 19:49:05 -0400

    Skeleton of 24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,41 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+
+public class Solution24 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+        //prompts user with string that was provided
+        //returns the users next line
+    }
+
+    private boolean isAnagram(String word1, String word2){
+        //create a function isAnagram that takes in two strings
+        //set all the letters to lower case
+        //sort them for both strings
+        //compare the new strings
+        //if they are the same returns true
+        //else returns false
+    }
+
+
+    public static void main(String[] args) {
+        Solution24 sol = new Solution24();
+        //prompt the user on the concept of the program
+
+        //calls the readInput and passes the prompt for the user to give a word
+        //store the value
+
+        //calls the readInput and passes the prompt for the user to give another word
+        //store the value
+
+        //call function isAnagram
+
+        //if the function returns true print they are anagrams
+        //else print they are not
+
+    }
+
+}
