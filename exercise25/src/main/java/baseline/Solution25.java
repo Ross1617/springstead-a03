274c9ee56a75181cae29dbf30705ee56cda40821
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-09-30 15:15:16 -0400

    working code 25

--- exercise25/src/main/java/baseline/Solution25.java
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -5,13 +5,17 @@
 
 package baseline;
 import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution25 {
     private static final Scanner in = new Scanner(System.in);
 
     private String readInput(String prompt){
         //prompts the user with the string that was provided
+        System.out.println(prompt);
         //returns the users next Line
+        return in.nextLine();
     }
 /*
 A very weak password contains only numbers and is fewer than eight characters.
@@ -21,59 +25,107 @@ A very strong password contains letters, numbers, and special characters and is 
  */
     private int passwordValidator (String password){
         //takes a password as the input and returns a number 1-5 depending on the strength
-
+        //char[] passwordArray = password.toCharArray();
         //create a variable that gets the length of the password
+        int length = password.length();
         //create a booleans that hold the value of if there is a number, letter, special character
+        boolean number = false, letter = false, specialCharacter = false;
+        for(int i = 0; i < length; i++){
+            //check to see if there are any letters
+            if ((Character.isLetter(password.charAt(i)) == true)) {
+                //if so set letter to true
+                letter = true;
+            }
+            //checks to see if are any numbers in the password
+            if((Character.isDigit(password.charAt(i)) == true)){
+                //if so set number to true
+                number = true;
+            }
+            //checks to see if there are any special characters
+            if (password.contains("@") == true || password.contains("!") == true || password.contains("#") == true|| password.contains("$") == true || password.contains("%") == true || password.contains("^") == true || password.contains("&") == true || password.contains("*") == true || password.contains("(") == true || password.contains(")") == true || password.contains("-") == true || password.contains("?") == true || password.contains("<") == true || password.contains(">") == true || password.contains("/") == true || password.contains(",") == true || password.contains(".") == true || password.contains("|") == true){
+                //if so sets specialCharacter to true
+                specialCharacter = true;
+            }
 
-        //checks to see if are any numbers in the password
-            //if so set number to true
-
-        //check to see if there are any letters
-            //if so set letter to true
-        
-        //checks to see if there are any special characters
-            //if so sets specialCharacter to true
-
+        }
         //if password is longer than eight keep going
+        if (length > 7){
             //check to see if the password has letters, numbers, and special character
+            if(number == true && letter == true && specialCharacter == true){
                 //if so return 4 for very strong
-            //else
-                //check to see if password has letters and numbers
-                    //if it does return 3
-                //else
-                    // return 5 for case where this result was not specified
-        //else
-            //if the password is only made out of numbers
-                //return 1 for the case of very weak password
-            //else
-                //if the password contains  special characters
-                    //return 5 for test case not defined
-                //else
-                    //return 2 for weak password
+                return 4;
+            }
+            //check to see if password has letters and numbers
+            if(number == true && letter == true ){
+                //if it does return 3
+                return 3;
+            }
+            else{
+                // return 5 for case where this result was not specified
+                return 5;
+            }
 
+        }
+        else{
+            //if the password is only made out of numbers
+            if(number == true && letter == false && specialCharacter == false){
+                //return 1 for the case of very weak password
+                return 1;
+            }
+            else {
+                //if the password contains  special characters
+                if (specialCharacter == true ){
+                    //return 5 for test case not defined
+                    return 5;
+                }
+                else{
+                    //return 2 for weak password
+                    return 2;
+                }
+            }
+        }
 
     }
     public static void main(String[] args) {
         Solution25 sol = new Solution25();
         //calls readInput with the prompt asking the user for a password
-
         //takes the users input and saves it to String
-
+        String password = sol.readInput("What is your password? ");
         //calls passwordValidator with the String that was provided
-
-        //create an empty String for the result
-
+        //creates variable for int return
+        int result = sol.passwordValidator(password);
+        //create a String for the result
+        String printResult = "The password ";
         //if the result is 1
+        if(result == 1){
             //add very weak password to result string
+            printResult = printResult + password + " is a very weak password.";
+        }
         //if the result is 2
+        if(result ==2){
             //add weak password to result string
-        //if the result is 3
-            //add strong password to result string
-        //if result is 4
-            //add very strong password to result string
-        //if result is 5
-            //add case not specified to result string
-        //print result string
+            printResult = printResult + password + " is a weak password.";
+        }
 
+        //if the result is 3
+        if(result ==3){
+            //add strong password to result string
+            printResult = printResult + password + " is a strong password.";
+        }
+
+        //if result is 4
+        if(result ==4){
+            //add very strong password to result string
+            printResult = printResult + password + " is a very strong password.";
+        }
+
+        //if result is 5
+        if(result ==5){
+            //add case not specified to result string
+            printResult = printResult + " is unknown strength.";
+        }
+
+        //print result string
+        System.out.printf("%s",printResult);
     }
 }
