66817caaf08169c6468a98c471efb536978dbb78
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-01 19:16:08 -0400

    skeleton code

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,27 @@
+package baseline;
+import java.util.Scanner;
+public class Solution30 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+
+        //prompts user with string that was provided
+
+        //returns the users next line
+
+    }
+    public int tableMaker(int height, int width){
+        //takes the height and does a for loop for it
+            //takes the width and does another for loop
+                //prints the number of counter
+                //increases counter by 1
+            //prints blank line
+        //returns 1 if sucessfull
+    }
+
+    public static void main(String[] args) {
+        //promtps the user for a number and saves the value
+        //prompts the user for another number and saves the value
+        //calls tableMaker with the two values
+
+    }
+}
