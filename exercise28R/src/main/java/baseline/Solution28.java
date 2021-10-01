abe56fe9985baf5c073480a71a2db7fca16863b3
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-09-30 23:33:43 -0400

    skeletonCode I had to add R

--- exercise28R/src/main/java/baseline/Solution28.java
+++ exercise28R/src/main/java/baseline/Solution28.java
@@ -4,21 +4,36 @@
  */
 
 package baseline;
-
+import java.util.Scanner;
 public class Solution28 {
+    private static final Scanner in = new Scanner(System.in);
     private String readInput(String prompt){
         //prompts the user for an int
+        System.out.println(prompt);
         //returns the user next INt
+        return in.nextLine();
     }
     public int counter(){
         //create a counter
+        int counter = 0;
+        int value = 0;
         //runs 5 times
-        // ask the user for a number by calling readINput
-        //counter += input
+        while (counter <5 ){
+            // ask the user for a number by calling readINput
+            //value += input
+            value += Integer.valueOf(readInput("Give me a number:"));
+            counter ++;
+        }
         //print the output counter
         //return counter
+        System.out.printf("The total is %d",value);
+        return value;
+
     }
     public static void main(String[] args) {
         //calls the int counter function
+        Solution28 sol = new Solution28();
+        sol.counter();
+
     }
 }
---
+++ exercise28R/src/test/java/baseline/Solution28Test.java
@@ -0,0 +1,15 @@
+package baseline;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class Solution28Test {
+    @Test
+    public void test_counter(){
+        Solution28 sol = new Solution28();
+        int value = sol.counter();
+
+
+    }
+
+}
