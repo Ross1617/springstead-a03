ecbe5fc1b06c08e8ba8dc5ed4dbeeda50cfe1b11
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-01 18:45:31 -0400

    skeleton code

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+import java.util.Scanner;
+public class Solution29 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+        //prompts user with string that was provided
+        //check the input if it is not a number or 0
+            //if its reprompts the user
+        //returns the users next line
+
+    }
+    public int calculate(int number){
+        //takes the input and uses the formula 72/ number
+        //prints the number of years
+        //returns the number of years
+    }
+
+    public static void main(String[] args) {
+        Solution29 sol = new Solution29();
+        //calls the readInput function
+        //and assigns the number to a variable
+        //calls the calculate function
+
+    }
+}
