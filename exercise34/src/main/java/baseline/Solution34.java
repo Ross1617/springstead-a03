aae70169c1ff8208588821e42ff0220ab578df2e
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-02 13:08:01 -0400

    skeleton code

---
+++ exercise34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,29 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ross Springstead
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Solution34 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+        //prompt the user
+        //return the user input
+    }
+    public ArrayList arrayListDelete(ArrayList listOfNames,String name){
+        //take the string name and delete it from the arrayList
+
+        //return the new ArrayList
+    }
+
+    public static void main(String[] args) {
+        //create the array list with all the names
+        //print the array list
+
+        //prompt the user what name they want deleted
+
+        //print the new array list
+    }
+
+}
