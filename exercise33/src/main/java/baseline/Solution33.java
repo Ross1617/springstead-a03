2b3d7b2b21b055b38d197726bbb8686deb49c88e
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-02 12:42:06 -0400

    skeleton code

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,32 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ross Springstead
+ */
+import java.util.Scanner;
+public class Solution33 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+        //prompt the user
+        //return the user input
+    }
+    public String magic8(int number){
+        String expression;
+
+        //depending on what the random number print a expression
+        //done by doing if statements
+
+
+        //return the expression
+    }
+
+    public static void main(String[] args) {
+        Solution33 sol = new Solution33();
+        //ask the user to enter a question
+        //generate a random number
+        //calls the magic8 function with that number
+        
+
+
+    }
+}
