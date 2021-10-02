9c60b3b2913358067a8384a7e4e6baede53fdb9a
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-02 13:47:51 -0400

    skeleton code

---
+++ exercise35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,34 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+public class Solution35 {
+
+    Random rand = new Random();
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+        //prompt the user
+        //make sure it isn't a blank string 
+
+        //return the user input
+    }
+    public String winnerPick(ArrayList names, int randomNumber){
+        //takes the string at the index
+        //returns the name chosen
+    }
+
+    public static void main(String[] args) {
+        Solution35 sol = new Solution35();
+        //ask the user how many names they want to add
+        //create an arrayList
+        //for a loop for however many names
+            //add the name
+        //calculate random number using amount of names
+        //call winnerPick
+    }
+
+}
