7b0ac5ebe8910eef898955b508aca6a8105b1231
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-01 21:23:53 -0400

    working code and test case

--- exercise30/src/main/java/baseline/Solution30.java
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -2,26 +2,53 @@ package baseline;
 import java.util.Scanner;
 public class Solution30 {
     private static final Scanner in = new Scanner(System.in);
-    private String readInput(String prompt){
+    private int readInput(String prompt){
 
         //prompts user with string that was provided
-
+        System.out.println(prompt);
         //returns the users next line
-
+        return in.nextInt();
     }
     public int tableMaker(int height, int width){
         //takes the height and does a for loop for it
+        int counter = 1;
             //takes the width and does another for loop
-                //prints the number of counter
-                //increases counter by 1
-            //prints blank line
+            for (int i = 0; i < height; i++){
+                for(int j =0 ; j< width;j++){
+                    //prints the number of counter
+                    if (counter < 10){
+                        System.out.printf("%d    ",counter);
+                    }
+                    else if (9<counter && counter<100){
+                        System.out.printf("%d   ",counter);
+                    }
+                    else{
+                        System.out.printf("%d  ",counter);
+                    }
+
+                    //increases counter by 1
+
+                    counter++;
+                }
+                //prints blank line
+                System.out.printf("\n");
+            }
+
+
+
         //returns 1 if sucessfull
+        return 1;
     }
 
     public static void main(String[] args) {
         //promtps the user for a number and saves the value
+        Solution30 sol = new Solution30();
+
         //prompts the user for another number and saves the value
+        int num1 = sol.readInput("Give me the height");
+        int num2 = sol.readInput("Give me the width");
         //calls tableMaker with the two values
+        sol.tableMaker(num1, num2);
 
     }
 }
---
+++ exercise30/src/test/java/baseline/Solution30Test.java
@@ -0,0 +1,17 @@
+package baseline;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class Solution30Test {
+    @Test
+    public void test_tableMaker() {
+        Solution30 sol = new Solution30();
+        int expectedValue = 1;
+        int value1 = sol.tableMaker(4,3);
+        assertEquals(value1,expectedValue);
+    }
+
+
+
+}
