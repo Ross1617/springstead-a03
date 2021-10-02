48bd07ce7d06f6b647d45c78d18c8e1246477fc3
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-02 11:53:40 -0400

    skeleton code

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,38 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+import java.util.Random;
+public class Solution32 {
+    Random rand = new Random();
+    private String readInput(String prompt){
+        //prompts the user with the prompt given
+        //takes the value and check to see if it is a number
+            //if it is not a number increase continue the loop
+        //returns the value of times that it asked the user
+        //returns the value entered
+    }
+    private String readYN(String prompt){
+        //prompts the user with prompt
+        //returns the string entered
+    }
+
+    private int game(int upperbound){
+        //create a counter
+        //generates a random number depending on the difficulty
+        //while loop that goes until the user gets the number
+        //call readInput
+            //increase counter for every Input the user enters
+        //print when the user amount of guesses
+
+    }
+
+    public static void main(String[] args) {
+        //have a while loop that goes until the user enters n
+        //prompt the user for the difficulty
+        //call the game function depending on the user input
+        //ask the user if they want to continue
+
+    }
+}
