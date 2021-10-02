c73d18e9e8183d5fae91bbd8cf84403bf4e0450f
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-02 10:53:47 -0400

    skeleton code

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,35 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Ross Springstead
+ */
+package baseline;
+import java.util.Scanner;
+public class Solution31 {
+    private static final Scanner in = new Scanner(System.in);
+    private  String readInput(String prompt){
+        //prompts the user for an input
+
+        //checks to see if the value is a positive number
+            //if not a positive number
+            //prompts the user again for a new number
+
+        //returns a positive number
+    }
+    public int karvonenHeartRate(int pulse, int age){
+        //use this formula that was provided
+        //TargetHeartRate = (((220 − age) − restingHR) × intensity) + restingHR
+        //define intensity
+        //printing formating
+        //for loop that runs betweeen 55 -95
+            //prints the intensity as a % and rate
+        //returns the last value for targetHeartRate
+    }
+
+    public static void main(String[] args) {
+        Solution31 sol = new Solution31();
+        //prompts the user for resting pulse
+        //prompts the user for age
+        //calls heartRate and prints the chart
+
+    }
+}
