c11c017075667028ae3ffa28db5c1702fc7ce06a
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-03 15:24:05 -0400

    skeleton code

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,31 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Ross Springstead
+ */
+import java.util.*;
+import java.util.Scanner;
+public class Solution38 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt){
+        //prompt the user
+        //return the user input
+    }
+    public Integer[] filterEvenNumbers(String values[]){
+        //creates an array to hold the values
+        //get the length of the input
+        //for the length of the input
+            //convert the numbers to integers
+            //use % and if it is even add it to the new array
+        //return the new array
+    }
+
+
+    public static void main(String[] args) {
+        //ask the user to enter numbers with spaces
+        //create a blank string array
+        // add each number into the array
+        //call the filterEvenNumbers
+        //print the results
+    }
+}
