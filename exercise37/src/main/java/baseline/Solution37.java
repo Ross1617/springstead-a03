ac241d6c864bdb87f14abdd3de4d9a4df02410c6
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-03 14:36:27 -0400

    skeleton code

---
+++ exercise37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,39 @@
+package baseline;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
+public class Solution37 {
+    Random rand = new Random();
+    private static final Scanner in = new Scanner(System.in);
+    private int readInt(String prompt){
+        //prompt the user
+        //return the user input
+    }
+
+    public String passwordCreator(int minLength, int special, int numbers){
+        //create a blank string
+        // add up special charactes and numbers
+        //make sure that the minLength is achieved
+        //create a variable for normalletters
+        //create list for normal, special, and numbers 
+        //for amount of normalletters
+            //randomly select a normal letter
+            //add it to the string
+        //for amount of specialLetter
+            //randomly select a special letter
+            //add it to the string
+        //for amount of numbers
+            //randomly select a number
+            //add it to the string
+        //returns the string
+    }
+
+    public static void main(String[] args) {
+        //prompt the user for minimum numbers
+        //prompt the user for amount of special characters
+        //prompt the user for amount of numbers
+        //call password function with all the parameters
+        //print the password
+
+    }
+}
