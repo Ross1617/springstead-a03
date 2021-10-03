a4faa7a7db5b6fe7189031e6ae47178afdd6438e
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-03 16:13:25 -0400

    working code and test case

--- exercise28R/src/main/java/baseline/Solution28.java
+++ exercise28R/src/main/java/baseline/Solution28.java
@@ -1,10 +1,13 @@
 /*
  *  UCF COP3330 Fall 2021 Assignment 3 Solutions
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Ross Springstead
  */
 
 package baseline;
+import java.util.ArrayList;
+
 import java.util.Scanner;
+//had to redesign
 public class Solution28 {
     private static final Scanner in = new Scanner(System.in);
     private String readInput(String prompt){
@@ -13,19 +16,18 @@ public class Solution28 {
         //returns the user next INt
         return in.nextLine();
     }
-    public int counter(){
-        //create a counter
-        int counter = 0;
-        int value = 0;
-        //runs 5 times
-        while (counter <5 ){
-            // ask the user for a number by calling readINput
-            //value += input
-            value += Integer.valueOf(readInput("Give me a number:"));
-            counter ++;
-        }
+    public int counter(ArrayList numbers){
+
         //print the output counter
+        int value = 0;
+        int length = numbers.size();
+        //for all 5 numbers
+        for (int i = 0; i < length; i++){
+            //adding them together
+            value += (int)numbers.get(i);
+        }
         //return counter
+        //length of ArrayList
         System.out.printf("The total is %d.",value);
         return value;
 
@@ -34,7 +36,21 @@ public class Solution28 {
     public static void main(String[] args) {
         //calls the int counter function
         Solution28 sol = new Solution28();
-        sol.counter();
+        //create a counter
+        int counter = 0;
+        int value = 0;
+        //runs 5 times
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        while (counter <5 ){
+            // ask the user for a number by calling readINput
+            //value is equal to the input
+            value = Integer.valueOf(sol.readInput("Give me a number:"));
+            //adding the input to array list
+            numbers.add(value);
+            //increasing counter
+            counter ++;
+        }
+        int number = sol.counter(numbers);
 
     }
 }
--- exercise28R/src/test/java/baseline/Solution28Test.java
+++ exercise28R/src/test/java/baseline/Solution28Test.java
@@ -3,11 +3,21 @@ package baseline;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 class Solution28Test {
     @Test
     public void test_counter(){
         Solution28 sol = new Solution28();
-        int value = sol.counter();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(20);
+        numbers.add(20);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(20);
+        int value = sol.counter(numbers);
+        int expected = 110;
+        assertEquals(value,expected);
 
 
     }
