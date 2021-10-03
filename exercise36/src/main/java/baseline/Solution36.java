19f0ec541a9a373b8c9ab11e5c425a36c08519de
Ross Springstead <ross.springstead@knights.ucf.edy>
2021-10-03 13:28:39 -0400

    skeleton code

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,50 @@
+package baseline;
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Solution36 {
+    private static final Scanner in = new Scanner(System.in);
+    private String readInput(String prompt) {
+        //prompt the user
+        //if they enter any sting besides done make them reenter input 
+        //return what they say
+    }
+    public double average(ArrayList listOfNumbers){
+        //create a value
+        //for every number in the array list add it up
+        //divide by the total amount of numbers
+        //return the amount
+    }
+    public double max(ArrayList listOfNumbers){
+        //for every index of the array list
+            //check to see if the index is the largest
+        //return the largest
+    }
+    public double min(ArrayList listOfNumbers){
+        //for every index of the array list
+            //check to see if the index is the smallest
+        //return the largest
+    }
+    public double std(ArrayList listOfNumbers){
+        //calculate the length
+        //calculate the sum of the arrayList by using a for loop
+        //calculate the mean
+        //use a for loop that takes each index of the list and applies the formula
+        //returns std
+    }
+
+
+    public static void main(String[] args) {
+        //create a while loop that runs until user enters done
+        //prompt the user to enter number
+        //if user enters done break the loop
+        //else add the number to the array list
+
+        //call average, max, min, and std
+
+    }
+
+}
